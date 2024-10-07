ffc3a7e54e0f3eaa1d862eb73b60ae9270d93be8
Zeeshan1405 <zeeshank14053@gmail.com>
2024-10-07 15:27:03 +0530

    zeeshan

--- program1.py
+++ program1.py
@@ -4,17 +4,23 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        pass
-
-
-
-
-
-
-
-    
-
-
-
-  
-
+        # Dictionary to map closing brackets to corresponding opening brackets
+        bracket_map = {')': '(', ']': '[', '}': '{'}
+        stack = []
+        
+        # Traverse the input string
+        for char in s:
+            # If the character is a closing bracket
+            if char in bracket_map:
+                # Pop the top element from the stack if it's not empty, else assign a dummy value
+                top_element = stack.pop() if stack else '#'
+                
+                # Check if the popped element is the matching opening bracket
+                if bracket_map[char] != top_element:
+                    return False
+            else:
+                # If it's an opening bracket, push it onto the stack
+                stack.append(char)
+        
+        # If the stack is empty, all opening brackets have been matched correctly
+        return not stack
--- program2.py
+++ program2.py
@@ -4,7 +4,26 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass
-
-
-
+        # Dictionary to map Roman numerals to their corresponding values
+        roman_map = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50, 
+            'C': 100, 'D': 500, 'M': 1000
+        }
+        
+        total = 0
+        prev_value = 0
+        
+        # Traverse the string in reverse
+        for char in reversed(s):
+            curr_value = roman_map[char]
+            
+            # If current value is less than the previous value, subtract it
+            if curr_value < prev_value:
+                total -= curr_value
+            else:
+                total += curr_value
+            
+            # Update previous value to the current one
+            prev_value = curr_value
+        
+        return total
